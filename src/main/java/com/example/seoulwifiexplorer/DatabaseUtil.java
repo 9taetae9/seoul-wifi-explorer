ae66cea7f0cf438e0791304856ab9e15395f2aba
TaeHyoun Kim <kth9903@gmail.com>
2024-02-07 22:03:52 +0900

    refactor databaseutil to use jdbc 4.0 auto-loading

--- src/main/java/com/example/seoulwifiexplorer/DatabaseUtil.java
+++ src/main/java/com/example/seoulwifiexplorer/DatabaseUtil.java
@@ -5,7 +5,15 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseUtil {
-    private static final String JDBC_URL = "jdbc:sqlite:seoulwifi.db";
+
+    static {
+        try {
+            Class.forName("org.sqlite.JDBC");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+    private static final String JDBC_URL = "jdbc:sqlite:/Users/taehyounkim/seoulwifi.db";
 
     public static Connection getConnection() throws SQLException{
         return DriverManager.getConnection(JDBC_URL);
