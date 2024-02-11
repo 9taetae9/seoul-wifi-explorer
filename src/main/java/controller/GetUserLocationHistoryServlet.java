a6765ead2e532fe6a52872fc115f0ad5a6ef455b
TaeHyoun Kim <kth9903@gmail.com>
2024-02-11 14:32:49 +0900

    feat: create endpoint to fetch user location history as json

---
+++ src/main/java/controller/GetUserLocationHistoryServlet.java
@@ -0,0 +1,24 @@
+package controller;
+
+import DAO.UserHistoryDAO;
+import DTO.UserHistory;
+import com.google.gson.Gson;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+public class GetUserLocationHistoryServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        List<UserHistory> history = new UserHistoryDAO().getUserLocationHistory();
+
+        // Convert history to JSON
+        String json = new Gson().toJson(history);
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+        response.getWriter().write(json);
+    }
+}
